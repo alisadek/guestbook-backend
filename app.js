4f18ac26193ff1df9649ec2e8f20dfa1213263da
alisadekk <alysadekk@gmail.com>
2020-05-16 10:06:45 +0200

    Added user authentication connected to the backend

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const checkAuth= require("./middleware/check-auth");
 const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
 
-
 const userSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  email: { type: String, require: true },
-  password: { type: String, require: true, minlength: 6 },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
   comments: [{ type: mongoose.Types.ObjectId, required: true, ref: "Comment" }],
 });
 const User = mongoose.model("User", userSchema);
 
 const commentSchema = new mongoose.Schema({
-  content: { type: String, require: true },
+  content: { type: String, required: true },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  author: { type: String, required: true },
 });
 const Comment = mongoose.model("Comment", commentSchema);
 
@@ -55,11 +57,8 @@ app
   /***************CREATE A COMMENT******************/
 
   .post(async function (req, res, next) {
-    const { content, creator } = req.body;
-    const createdComment = new Comment({
-      content,
-      creator,
-    });
+    const { content, creator, author } = req.body;
+
     let user;
     try {
       user = await User.findById(creator);
@@ -74,6 +73,11 @@ app
       const error = new HttpError("Could not find user for provided id", 404);
       return next(error);
     }
+    const createdComment = new Comment({
+      content,
+      author,
+      creator,
+    });
     console.log(user);
     try {
       await createdComment.save();
@@ -97,7 +101,7 @@ app
       );
       return next(error);
     }
-
+    console.log(createdComment);
     res.status(201).json({ comment: createdComment });
   });
 
@@ -132,6 +136,29 @@ app
   /*******************************FETCH A COMMENT*********************/
   .get(async function (req, res, next) {
     const commentId = req.params.cid;
+    let edittedComment;
+    try {
+      edittedComment = await Comment.findById(commentId);
+    } catch (err) {
+      const error = new HttpError(
+        "Something Went Wrong, Could not find comment",
+        500
+      );
+      return next(error);
+    }
+    if (!edittedComment) {
+      const error = new HttpError(
+        "Could not find a comment with the selected id.",
+        404
+      );
+      return next(error);
+    }
+    res.json({ edittedComment: edittedComment.toObject({ getters: true }) });
+  })
+  /*******************************EDIT COMMENT*********************/
+  .patch(async function (req, res, next) {
+    const content = req.body.content;
+    const commentId = req.params.cid;
     let foundComment;
     try {
       foundComment = await Comment.findById(commentId);
@@ -149,19 +176,15 @@ app
       );
       return next(error);
     }
-    res.json({ foundComment: foundComment.toObject({ getters: true }) });
-  })
-  /*******************************EDIT COMMENT*********************/
-  .patch(function (req, res, next) {
-    const content = req.body.content;
-    const commentId = req.params.cid;
-    let comment = Comment.findById(commentId, (err, foundComment) => {
-      foundComment.content = content;
-      foundComment.save();
-      res
-        .status(200)
-        .json({ foundComment: foundComment.toObject({ getters: true }) });
-    });
+    foundComment.content = content;
+    try {
+      await foundComment.save();
+    } catch (err) {
+      const error = new HttpError("Comment Edit Failed, please try again,");
+    }
+    res
+      .status(200)
+      .json({ foundComment: foundComment.toObject({ getters: true }) });
   })
 
   /*******************************DELETE COMMENT*********************/
@@ -196,12 +219,48 @@ app
       );
       return next(error);
     }
-    if (!existingUser || existingUser.password !== password) {
+    if (!existingUser) {
       const error = new HttpError("Invalid Username or Password", 401);
       return next(error);
     }
 
-    res.json({ message: "Logged in!" });
+    let isValidPassword = false;
+    try {
+      isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+      const error = new HttpError("Login Failed. Please try again.", 500);
+      return next(error);
+    }
+    if (!isValidPassword) {
+      const error = new HttpError("Invalid Username or Password", 401);
+      return next(error);
+    }
+
+    let token;
+    try {
+      token = jwt.sign(
+        {
+          userId: existingUser.id,
+          email: existingUser.email,
+          name: existingUser.name,
+        },
+        "ali_coformatique_secret",
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      const error = new HttpError(
+        "logging in failed, please try again later.",
+        500
+      );
+      return next(error);
+    }
+
+    res.json({
+      userId: existingUser.id,
+      email: existingUser.email,
+      name: existingUser.name,
+      token: token,
+    });
   });
 
 /*******************************SIGNUP ROUTE*********************/
@@ -226,10 +285,18 @@ app.route("/api/signup").post(async function (req, res, next) {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+     hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create user, please try again", 500);
+    return next(error);
+  }
   const createdUser = new User({
     name,
     email,
-    password,
+    password: hashedPassword,
+    comments: [],
   });
 
   try {
@@ -238,13 +305,33 @@ app.route("/api/signup").post(async function (req, res, next) {
     const error = new HttpError("Signing up failed, please try again.", 500);
     return next(error);
   }
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
-});
 
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could not find this route.", 404);
-//   throw error;
-// });
+  let token;
+  try {
+    token = jwt.sign(
+      {
+        userId: createdUser.id,
+        email: createdUser.email,
+        name: createdUser.name,
+      },
+      "ali_coformatique_secret",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(201).json({
+    userId: createdUser.id,
+    email: createdUser.email,
+    name: createdUser.name,
+    token: token,
+  });
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
